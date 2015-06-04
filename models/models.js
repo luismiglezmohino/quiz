0017fe206b88d800d706b6f4a88400b214d9903f
Luis Miguel González-Mohino <luismi.glez.mohino@gmail.com>
2015-06-04 20:48:55 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]|| null);
+var user = (url[2]|| null);
+var pwd = (url[3]|| null);
+var protocol = (url[1]|| null);
+var dialect = (url[1]|| null);
+var port = (url[5]|| null);
+var host = (url[4]|| null);
+var storage = process.env.DATABASE_STORAGE;
+
+console.error( "Base de Datos: "+DB_name+ " user "+user +" pwd "+pwd+ "protocol "+protocol+" dialect "+dialect+" port "+port+ "host "+host+" storage "+storage);
+//Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar BBDD SQLite:
+var sequelize = new Sequelize(DB_name,user,pwd,
+                              {
+                                dialect:protocol,
+                                protocol: protocol,
+                                port: port,
+                                host: host,
+                                storage:storage,
+                                omitNull: true
+                            });
+
+//Importar la definicion de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+exports.Quiz = Quiz; //exportar definición de tabla Quiz
+//sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function(){
+   Quiz.count().then(function(count){
+     if(count === 0){
+         Quiz.create({
+             pregunta: '¿Capital de Italia?',
+             respuesta: 'Roma'
+         }).then(function(){
+             console.log('Base de datos inicializada');
+         })
+     }
+   });
+});
