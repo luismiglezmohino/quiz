30531b1bec8ae1ab822b2a3defb795c632232629
Luis Miguel González-Mohino <luismi.glez.mohino@gmail.com>
2015-06-10 19:49:48 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -85,3 +85,9 @@ exports.update = function(req,res){
        }
     });
 }
+
+exports.destroy = function(req,res){
+    req.quiz.destroy().then(function(){
+        res.redirect('/quizes');
+    }).catch(function(error){next(error);});
+}
