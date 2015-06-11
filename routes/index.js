dce55e8f64aabb108abf0d09b98012d5f677c861
Luis Miguel González-Mohino <luismi.glez.mohino@gmail.com>
2015-06-11 20:45:26 +0200

    Crear Comentario

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var authorController = require('../controllers/author_controller');
+var commentController = require('../controllers/comment_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' ,errors:[]});
@@ -20,4 +21,8 @@ router.post('/quizes/create',quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
 router.put('/quizes/:quizId(\\d+)',quizController.update);
 router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+
 module.exports = router;
