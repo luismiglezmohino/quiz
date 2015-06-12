18099f369513f3992265e5878e77e62e9dac4894
Luis Miguel González-Mohino <luismi.glez.mohino@gmail.com>
2015-06-12 19:22:49 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load);
 
 //Definicion de rutas de sesion
 router.get('/login',sessionController.new);
@@ -32,5 +33,6 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizContro
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired,commentController.publish);
 
 module.exports = router;
